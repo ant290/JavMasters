e74f30b860374c72e525731e4bcd0a5a95e24d05
Ant290 <ant290@live.co.uk>
2018-11-10 23:46:42 +0000

    initialised Maven project

---
+++ JavMasters/src/main/java/com/SendyInd/JavMasters/App.java
@@ -0,0 +1,13 @@
+package com.SendyInd.JavMasters;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
